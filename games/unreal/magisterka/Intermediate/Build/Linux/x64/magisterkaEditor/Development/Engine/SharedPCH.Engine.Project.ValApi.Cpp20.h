749568a469fb0b06095b3cae7caa882967ce8e5f
Krzysztof kuhy Rudnicki <krzysztofrudnicki0@gmail.com>
2025-11-06 20:41:06 +0100

    chore: added black unreal engine project

---
+++ games/unreal/magisterka/Intermediate/Build/Linux/x64/magisterkaEditor/Development/Engine/SharedPCH.Engine.Project.ValApi.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Engine/Public/EngineSharedPCH.h
+#include "/home/kuhy/Unreal Projects/magisterka/Intermediate/Build/Linux/x64/magisterkaEditor/Development/Engine/SharedDefinitions.Engine.Project.ValApi.Cpp20.h"
+#include "Runtime/Engine/Public/EngineSharedPCH.h"
